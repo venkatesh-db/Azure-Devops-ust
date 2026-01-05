e31b60d485edfb81e67118adb2d90e0e74021dec
venkatesh db <54406318+venkatesh-db@users.noreply.github.com>
2026-01-05 11:33:25 +0000

    Add files via upload

---
+++ Day1-Python-Handson/smile2.py
@@ -0,0 +1,15 @@
+
+import smile1 #create variable --> file 
+
+smile1.Reuse()
+
+smile1.generticcoders("roti")
+
+
+from azure_devops import smile1
+
+#     folder              file 
+
+
+smile1.Reuse()
+# file execute function
