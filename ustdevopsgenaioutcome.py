5f76ce607b520c7c4b87822690c299fc213939f2
venkatesh db <54406318+venkatesh-db@users.noreply.github.com>
2026-01-20 10:32:56 +0000

    Add files via upload

---
+++ ustdevopsgenaioutcome.py
@@ -0,0 +1,108 @@
+
+# 1. job / change initiagtion stage 
+
+ new code deployment 
+ config change 
+ infra change 
+ 
+#2. build & validate stage 
+
+build docker /binaries 
+unit tests
+Liniting 
+securiry scan 
+
+#3.deploy to non-prod safe zone 
+
+smoke tests
+api health checks
+integration tests
+performance tests
+security tests
+user acceptance tests
+
+#4. traffic strategy stage   
+
+blue green deployment
+blue-old
+green - new 
+
+canary deployment
+step by step traffic shift
+
+#5. Live traffic 
+
+load balancing 
+service mesh 
+azure traffic manager 
+
+#6. monitoring & Observablity stage
+
+sucess or roolback decision
+application monitoring
+infrastructure monitoring
+log management
+
+
+#7. decision gate stage
+
+based on metrics and kpis
+proceed to next stage or roolback
+
+
+#8.Rollback stage 
+
+rollback must be fast & automoted 
+rollback : 2-5 time 
+
+#9. Post deployment validation stage
+
+confirm stablity 
+Long running mointoring 
+User Complaints
+
+
+#10. Project Learning 
+
+post mortem analysis
+incident rca 
+continuous improvement
+
+'''
+
+ AI-Powered Prompt Engineering for Automation Tasks 1- 9
+Configuration Management -
+
+ AI Integration: Root Cause Detection on failed pipeline runs - Logs 
+ 
+ Pipeline Debugging using Python & AI Log Summarizers
+ 
+ Root Cause Analysis (RCA) Automation using AI-based summarizers
+ 
+ Predicting deployment failures using Machine Learning models
+ 
+ ML for DevOps
+ 
+ '''
+
+
+AI - agents 
+
+# Rollback stage 
+# decision gate stage
+# monitoring & Observablity stage
+
+
+ML for DevOps
+
+#  Post deployment validation stage
+#  Project Learning 
+#  monitoring & Observablity stage
+# traffic strategy stage
+
+# new requirement
+
+ #1. job / change initiagtion stage 
+ #2. build & validate stage 
+ #3. deploy to non-prod safe zone 
+
